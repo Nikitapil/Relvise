b97ee91b05ed98314a7084b3b259988469403291
Nikitapil <nikitapiliavets@MacBook-Pro-Nikita.local>
2021-11-12 00:03:19 +0300

    add services and experience sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,24 @@
-import '../style/style.scss'
+import "../style/style.scss";
 
 //mobile menu
 
-const mobileBtn = document.querySelector('.mobile-menu-btn')
-const navList = document.querySelector('.header__nav-list')
-document.addEventListener("click", (event)=> {
-    let targetEl = event.target
-    if (targetEl.closest('.mobile-menu-btn') || targetEl.closest('.header__nav-item')) {
-        mobileBtn.classList.toggle('active-mobile')
-        navList.classList.toggle('active-mobile')
-    }
-})
+const mobileBtn = document.querySelector(".mobile-menu-btn");
+const navList = document.querySelector(".header__nav-list");
+document.addEventListener("click", (event) => {
+  let targetEl = event.target;
+  if (
+    targetEl.closest(".mobile-menu-btn") ||
+    targetEl.closest(".header__nav-item")
+  ) {
+    mobileBtn.classList.toggle("active-mobile");
+    navList.classList.toggle("active-mobile");
+  }
+});
+window.addEventListener("scroll", () => {
+  const header = document.querySelector('.header');
+  if (window.pageYOffset > 0) {
+    header.classList.add('header-bg');
+  } else {
+    header.classList.remove('header-bg');
+  }
+});
